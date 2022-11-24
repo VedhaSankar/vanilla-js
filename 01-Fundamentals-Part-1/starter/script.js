d7555e8dc71089925158ae74e8898af0f52dd227
VedhaSankar <vedhasankar26@gmail.com>
2022-11-25 00:17:16 +0530

    Updates for 00:17:16   25/11/22

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,10 @@
 let js = "vanilla"
 if (js === "vanilla") alert(js)
 console.log(90 + 45 + 4 + 5)
+
+let Name = "Green"
+console.log(Name)
+console.log(Name)
+console.log(Name)
+console.log(Name)
+console.log('dadada')
